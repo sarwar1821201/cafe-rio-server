d3e8e27cea7b0006b6d9d40ff3a726dee8728cbd
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-13 15:52:38 +0600

    basic setup for server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("restuarnt server is running");
+  });
+  
+  app.listen(port, () => {
+    console.log(`server running on : ${port}`);
+  });
