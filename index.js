b9fc18ed9e46ac3c3500d2ea30224c0d1ae0be54
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-13 16:11:20 +0600

    get menu and review collection data from database

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 //   "mongodb+srv://<username>:<password>@cluster0.w8gsdns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const uri =
-`mongodb+srv://${process.env.DB_USER}:${procees.env.DB_PASS}@cluster0.w8gsdns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w8gsdns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,13 +29,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const database = client.db("cafeRioDb");
+    const menuCollection = database.collection("menu");
+    const reviewsCollection = client.db("cafeRioDb").collection("reviews");
+
+    
+    app.get("/menu", async (req, res) => {
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+      });
+  
+      app.get("/reviews", async (req, res) => {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result);
+      });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
